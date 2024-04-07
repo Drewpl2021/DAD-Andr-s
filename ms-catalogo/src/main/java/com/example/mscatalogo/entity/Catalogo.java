1286d812730cb4405ca6891ada377d7a603b56fe
Drewpl2021 <linoplas0@gmail.com>
2024-04-06 20:02:27 -0500

    Producto

--- ms-catalogo/src/main/java/com/example/mscatalogo/entity/Catalogo.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/entity/Catalogo.java
@@ -1,10 +1,9 @@
 package com.example.mscatalogo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -13,4 +12,7 @@ public class Catalogo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
+
+    @OneToMany(mappedBy = "catalogo", cascade = CascadeType.ALL)
+    private List<Producto> productos;
 }
